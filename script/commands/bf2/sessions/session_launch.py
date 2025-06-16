efeb7bef4f086652a794005a1d16cb43f5dd5c7e
Wivex <213932052+Wivexx@users.noreply.github.com>
2025-06-16 20:57:17 +0200

    Update session_launch.py

--- script/commands/bf2/sessions/session_launch.py
+++ script/commands/bf2/sessions/session_launch.py
@@ -4,7 +4,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import datetime
-from script.commands.bf2.USEFUL_IDS import ID_ROLE_REPUBLIQUE, ID_ANNONCE_SESSION, ID_ROLE_LANCEUR, CHECK_GREEN_REACT, LATE_REACT, RED_CROSS_REACT, IDK_REACT
+from script.commands.bf2.USEFUL_IDS import ID_ROLE_REPUBLIQUE, ID_ANNONCE_SESSION, ID_ROLE_LANCEUR, CHECK_GREEN_REACT, LATE_REACT, RED_CROSS_REACT, IDK_REACT, session_pics
 
 
 class CommandeSessionLauncher(commands.Cog):
@@ -59,13 +59,6 @@ class CommandeSessionLauncher(commands.Cog):
                 ))
 
         embed.set_footer(text=f"Session lancée par {interaction.user}", icon_url=interaction.user.display_avatar.url)
-        session_pics = [
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EYnEMKDumO_MzcqSicvv7gHaDt%26pid%3DApi&f=1&ipt=6985444a0cf7c83e83c8b051c7e843f2dc8f6a19b0abc7739ca2c2ec24428c91&ipo=images",
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.gjmozccI4rSTnt3GRfOd9QHaEK%26pid%3DApi&f=1&ipt=c4b96d57a0ca49e2dfb050f9847758c92e21c1d5499ac241496acf9d21ed4517&ipo=images",
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.XtXlNCZ5Ao6jpXIvo33PDAHaEK%26pid%3DApi&f=1&ipt=c2782efe38919d9e0090ae7bb14cfa6f3bd92aaa8ecfa10da5c3eee6e597ccc6&ipo=images",
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.Dnp2-Gex2LcaQosdbqKLmQHaDt%26pid%3DApi&f=1&ipt=d7c3689de14ea7540e19f17c060c90bd999c2f5a9195887df0764d21cd07ad43&ipo=images",
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.yzKbQbDV5aXuQwTWN0HKmwHaEK%26pid%3DApi&f=1&ipt=122ea1110b58d2758210667f5c82b0c233f6576948ffad0c3dfb5fd5e7e485d4&ipo=images",
-            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.R7QYN-TV52MeZ_yQw3NNogHaEK%26pid%3DApi&f=1&ipt=46181f99f47209cfe65c5a02f6a754a0a898c07b9dcc409fcd7b56e706379765&ipo=images"]
         embed.set_image(url=random.choice(session_pics))
 
         await interaction.response.send_message(
