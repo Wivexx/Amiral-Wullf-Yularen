3cbaf25644b6894c1e880a0bc645e09ce02118b3
Wivex <213932052+Wivexx@users.noreply.github.com>
2025-06-25 09:08:35 +0200

    Update black_list.py

--- script/commands/bf2/moderation/black_list/black_list.py
+++ script/commands/bf2/moderation/black_list/black_list.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from script.commands.bf2.USEFUL_IDS import ID_ROLE_STAFF, ID_LOGS
+from USEFUL_IDS import ID_ROLE_STAFF, ID_LOGS
 import os
 
 current_dir = os.path.dirname(__file__)
